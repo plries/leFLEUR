ffa7e999f31e92e2f6346b8b8ee8ba3d933a8978
plries <apaulries@gmail.com>
2024-03-16 13:29:37 -0700

    finished filters

--- js/catalog.js
+++ js/catalog.js
@@ -1,3 +1,6 @@
+let allSections = document.getElementsByClassName("showHide");
+let Section = document.getElementsByClassName("catalogSection");
+
 function allFilter() {
 
   for(let i = 0;i < allSections.length; i++){
@@ -7,17 +10,65 @@ function allFilter() {
 
 }
 
-let allSections = document.getElementsByClassName("showHide")
-
-console.log(allSections)
-
 function topsFilter() {
 
   for(let i = 0;i < allSections.length; i++){
     allSections[i].style.display = "none";
 }
-  setTimeout(function() {
-    
-  })
+    allSections[0].style.display = "block";
+    Section[0].style.display = "flex";
+};
 
+function botsFilter() {
+
+  for(let i = 0;i < allSections.length; i++){
+    allSections[i].style.display = "none";
 }
+    allSections[1].style.display = "block";
+    Section[1].style.display = "flex";
+};
+
+function fragFilter() {
+
+  for(let i = 0;i < allSections.length; i++){
+    allSections[i].style.display = "none";
+}
+    allSections[2].style.display = "block";
+    Section[2].style.display = "flex";
+};
+
+function nailFilter() {
+
+  for(let i = 0;i < allSections.length; i++){
+    allSections[i].style.display = "none";
+}
+    allSections[3].style.display = "block";
+    Section[3].style.display = "flex";
+};
+
+function acceFilter() {
+
+  for(let i = 0;i < allSections.length; i++){
+    allSections[i].style.display = "none";
+}
+    allSections[4].style.display = "block";
+    Section[4].style.display = "flex";
+};
+
+function luggFilter() {
+
+  for(let i = 0;i < allSections.length; i++){
+    allSections[i].style.display = "none";
+}
+    allSections[5].style.display = "block";
+    Section[5].style.display = "flex";
+};
+
+function footFilter() {
+
+  for(let i = 0;i < allSections.length; i++){
+    allSections[i].style.display = "none";
+}
+    allSections[6].style.display = "block";
+    Section[6].style.display = "flex";
+};
